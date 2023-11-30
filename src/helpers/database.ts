fbfa7ceab497180abe32348ed5e6a21a054009e0
SvenSven <mikorecare@gmail.com>
2023-11-30 20:42:19 +0800

    Added template

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -14,4 +14,34 @@ const EmployeeSchema: Schema = new Schema({
 
 const Employee = mongoose.model<Employee>('Employee', EmployeeSchema);
 
+interface Article extends Document{
+  id: string;
+  url: string;
+  author: string;
+  publishedAt: Date;
+  title: string;
+  type: string;
+  featureImgUrl?: string;
+  description?: string;
+  videoUrl?: string;
+  adBannerUrl?: string;
+}
+
+const articleSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  url: { type: String, required: true },
+  author: { type: String, required: true },
+  publishedAt: { type: Date, required: true },
+  title: { type: String, required: true },
+  type: { type: String, required: true },
+  featureImgUrl: { type: String },
+  description: { type: String },
+  videoUrl: { type: String },
+  adBannerUrl: { type: String },
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
+
 export default Employee;
